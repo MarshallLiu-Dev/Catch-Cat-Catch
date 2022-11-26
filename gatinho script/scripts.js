85f9ce9cf211240f5b8db5cd95e714c515e9251d
MarshallLiu-Dev <paulomartins12358@gmail.com>
2022-11-25 23:36:38 -0300

    frist commit main

--- gatinho script/scripts.js
+++ gatinho script/scripts.js
@@ -3,13 +3,28 @@
 function toggle_cat(){
     let cat1 = document.querySelector("#cat1");
     let cat2 = document.querySelector("#cat2");
+    let nomeCat1 = document.querySelector("#nomeCat1");
+    let nomeCat2 = document.querySelector("#nomeCat2");
 
 
     if(cat1.style.display != 'none'){
         cat1.style.display = 'none';
         cat2.style.display = 'inline';
+        nomeCat1.style.display = "none";
+        nomeCat2.style.display = "inline";
     } else{
         cat1.style.display = 'inline';
         cat2.style.display = 'none';
+        nomeCat1.style.display = "inline";
+        nomeCat2.style.display = "none";
     }
 }
+/*
+
+ if (nomCat1.style.display != "none") {
+   nomCat1.style.display = "none";
+   nomeCat2.style.display = "inline";
+ } else {
+   nomeCat1.style.display = "inline";
+   nomfeCat2.style.display = "none";
+ }*/
